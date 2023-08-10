e1ce2863ddc265c608e6dd80aa8875e3129cc21f
Paul Holt <paul-ho@lightflows.co.uk>
2023-08-10 01:33:32 +0100

    Reorganise selection tool ready for modes

--- src/canvas/tools/SelectionTool.ts
+++ src/canvas/tools/SelectionTool.ts
@@ -2,37 +2,70 @@ import CanvasToolInterface from "./CanvasToolInterface";
 import Canvas from "../Canvas";
 
 class SelectionTool implements CanvasToolInterface {
-
     public selectedObject: ShapeInterface | null = null;
-    mouseDown(event: any, canvas: Canvas): void
+
+    private _mode: number = 0;
+
+    private _modes: string[] = ['move', 'resize'];
+
+    public mouseDown(event: any, canvas: Canvas): void
     {
         if(this.selectedObject !== null) {
-            this.selectedObject.draw(canvas.ctx);
-            canvas.canvasObjects.push(this.selectedObject);
-            this.selectedObject = null;
-
+            this.deselectObject(canvas);
             return;
         }
 
 
         canvas.canvasObjects.reverse().every((canvasObject: ShapeInterface) => {
             if(canvasObject.isPointInside(event.clientX, event.clientY)) {
-                console.log('Clicked on object ' + canvasObject);
-
                 this.selectedObject = canvasObject;
 
                 const index = canvas.canvasObjects.indexOf(canvasObject);
                 canvas.canvasObjects.splice(index, 1);
 
                 this.selectedObject.draw(canvas.ctx, 'blue');
-            }
 
-            return;
+                return;
+            }
         });
     }
 
     // @ts-ignore
-    mouseMove(event: any, canvas: Canvas): void
+    public mouseMove(event: any, canvas: Canvas): void
+    {
+        if(this.selectedObject === null) return;
+
+        if(this._modes[this._mode] === 'move') {
+            this.moveObject(event, canvas);
+        }
+    }
+
+    // @ts-ignore
+    public mouseUp(event: any, canvas: Canvas): void
+    {}
+
+    public keyDown(event: any, canvas: Canvas): void
+    {
+        if (event.key === "s" || event.key === "S") {
+            console.log('Selection tool selected');
+            canvas.selectedTool = this;
+        }
+
+        if (event.key === "m" || event.key === "M" ) {
+            this.switchMode();
+        }
+    }
+
+    private deselectObject(canvas: Canvas): void
+    {
+        if(this.selectedObject !== null) {
+            this.selectedObject.draw(canvas.ctx);
+            canvas.canvasObjects.push(this.selectedObject);
+            this.selectedObject = null;
+        }
+    }
+
+    private moveObject(event: any, canvas: Canvas): void
     {
         if(this.selectedObject !== null) {
             this.selectedObject.setPosition(event.clientX, event.clientY);
@@ -41,16 +74,20 @@ class SelectionTool implements CanvasToolInterface {
         }
     }
 
-    // @ts-ignore
-    mouseUp(event: any, canvas: Canvas): void
-    {}
-
-    keyDown(event: any, canvas: Canvas): void
+    private switchMode(): void
     {
-        if (event.key === "m" || event.key === "M") {
-            console.log('Selection mode');
-            canvas.selectedTool = this;
+        if(this.selectedObject !== null) {
+            console.log('Cannot switch mode while object is selected');
+            return;
         }
+
+        if(this._mode + 1 > this._modes.length - 1) {
+            this._mode = 0;
+        } else {
+            this._mode++;
+        }
+
+        console.log('Mode changed to: ' + this._modes[this._mode]);
     }
 }
 
