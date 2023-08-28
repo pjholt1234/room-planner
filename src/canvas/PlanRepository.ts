b8069426bfc676e2e740b7a0b0fa8e7b5a4e841c
Paul Holt <paul-ho@lightflows.co.uk>
2023-08-28 16:27:18 +0100

    Create plan repository

---
+++ src/canvas/PlanRepository.ts
@@ -0,0 +1,35 @@
+import ApiClient from './ApiClient';
+
+class PlanRepository {
+    private baseUrl: string = '';
+
+    constructor(public apiClient: ApiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public async savePlan(data: any): Promise<void> {
+        await this.apiClient.post(`${this.baseUrl}/create`, data);
+    }
+
+    public async loadPlan(id: string): Promise<any> {
+        const response: any[] = await this.apiClient.get(
+            `${this.baseUrl}/${id}`
+        );
+
+        if (response.length > 0) {
+            return response[0];
+        }
+
+        return response;
+    }
+
+    public async deletePlan(id: string): Promise<any> {
+        return await this.apiClient.delete(`${this.baseUrl}/delete/${id}`);
+    }
+
+    public async loadAllPlans(): Promise<void> {
+        return await this.apiClient.get(`${this.baseUrl}`);
+    }
+}
+
+export default PlanRepository;
